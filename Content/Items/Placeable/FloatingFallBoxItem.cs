480b18ad91bf7c899dee73b3945e97428e49b1c5
GabeHasWon <gabriel@guevarameyer.com>
2024-05-17 15:39:10 -0500

    1.2.2.3

---
+++ Content/Items/Placeable/FloatingFallBoxItem.cs
@@ -0,0 +1,20 @@
+using Arbour.Content.Tiles.Multitiles;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace Arbour.Content.Items.Placeable;
+
+public class FloatingFallBoxItem : ModItem
+{
+	public override void SetStaticDefaults()
+	{
+		Item.ResearchUnlockCount = 1;
+		MusicLoader.AddMusicBox(Mod, MusicLoader.GetMusicSlot(Mod, "Assets/Music/DayOld"), ModContent.ItemType<FloatingFallBoxItem>(), ModContent.TileType<FloatingFallBox>());
+	}
+
+	public override void SetDefaults()
+	{
+		Item.DefaultToPlaceableTile(ModContent.TileType<FloatingFallBox>());
+		Item.rare = ItemRarityID.LightRed;
+	}
+}
---
+++ Content/Tiles/Multitiles/MusicBoxes.cs
@@ -0,0 +1,11 @@
+﻿using Arbour.Content.Items.Placeable;
+
+namespace Arbour.Content.Tiles.Multitiles;
+
+public class StarlitWindsBox : BaseMusicBox<StarlitWindsBoxItem>
+{
+}
+
+public class FloatingFallBox : BaseMusicBox<FloatingFallBoxItem>
+{
+}
